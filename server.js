a06ac687c8e1e175ab9e9f4ef36097ee431e27c3
Tim Perry <tim.perry@bbc.co.uk>
2017-06-04 21:10:08 +0100

    Adding cors support

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
 
 const helpers = require('./helpers')
 
+app.use(cors())
+
 app.use('/', graphqlHTTP({
   schema,
   graphiql: true
-}));
+}))
 
 app.listen(4242, function () {
   helpers.log('info', 'Server started on port 4242')
